ee8be5fed1762aec68c4c5e87ca084f2d2b2ca1b
polinamladenova <polinamladenova91@gmail.com>
2023-08-11 18:33:58 +0300

    Fetched data from JSON file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//Toast trigger function=============
 const toastTrigger = document.getElementById("liveToastBtn");
 const toastLiveExample = document.getElementById("liveToast");
 
@@ -7,3 +8,11 @@ if (toastTrigger) {
     toastBootstrap.show();
   });
 }
+
+function showInfo(data) {
+  console.table(data.products[0].name);
+}
+
+fetch("/src/plant-data.json")
+  .then((response) => response.json())
+  .then((data) => showInfo(data));
